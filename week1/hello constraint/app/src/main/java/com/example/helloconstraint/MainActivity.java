adac1665cf367dfbabf2dd18081ad4a60d4829d9
SaminShrestha <shrsamin97@gmail.com>
2019-02-17 17:17:08 +0545

    hello constraint assignment

---
+++ week1/hello constraint/app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.helloconstraint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mShowCount;
+    private int mCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void show_toast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void nullify(View view) {
+        mShowCount.setText(Integer.toString(0));
+    }
+
+    public void showCount(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
